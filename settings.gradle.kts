12ac0a54ec55bf8f109a9cb634cf0829360a4416
Landon Barrett <lbarrett@stu.jsu.edu>
2024-01-23 13:30:27 -0600

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CS 408 Lab1 Mystery Helper"
+include(":app")
