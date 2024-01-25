cc4889a0ccf6783f7413bed55baf7b1a8fcfbf0c
Landon Barrett <lbarrett@stu.jsu.edu>
2024-01-25 13:44:58 -0600

    Button feature works. Text is displayed correctly. Final testing needs to be done.

--- app/src/main/java/com/example/cs408lab1mysteryhelper/MainActivity.java
+++ app/src/main/java/com/example/cs408lab1mysteryhelper/MainActivity.java
@@ -1,20 +1,34 @@
 package com.example.cs408lab1mysteryhelper;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cs408lab1mysteryhelper.databinding.ActivityMainBinding;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    private String[] storyPoint;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+        Random rand = new Random();
+        storyPoint = getResources().getStringArray(R.array.storyPoints);
+        binding.getHelpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int randInt = rand.nextInt(storyPoint.length + 1);
+                TextView t = binding.output;
+                t.setText(storyPoint[randInt]);
+            }
+        });
     }
 
 }
